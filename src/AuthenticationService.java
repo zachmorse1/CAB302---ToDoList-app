e3556ebcb6e3a67b804ebad48f3abe9d63ab9efa
Zach Morse <zach.morse@connect.qut.edu.au>
2025-03-05 19:43:28 +1000

    Created AuthenticationService class and started to add code.

---
+++ src/AuthenticationService.java
@@ -0,0 +1,22 @@
+public class AuthenticationService implements IAuthenticationService {
+    private ArrayList<User> users;
+
+    public AuthenticationService() {
+
+    }
+
+
+    public User signUp(String username, String password) {
+        return null;
+    }
+
+    public User logIn(String username, String password) {
+        return null;
+    }
+    // TODO Now: Add a constructor to initialize the users list with the default user
+
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+
+    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+}
+
