4888be1a0199216243ec6d62b899b96dae6264fa
Zach Morse <zach.morse@connect.qut.edu.au>
2025-03-09 14:15:33 +1000

    Completed Student 2 - Authentication branch.

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -5,11 +5,12 @@ import java.util.Objects;
 public class AuthenticationService implements IAuthenticationService {
     private ArrayList<User> users;
 
+    // TODO Now: Add a constructor to initialize the users list with the default user
     public AuthenticationService(ArrayList<User> users) {
         this.users = users;
     }
 
-
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
     public User signUp(String username, String password) {
         Iterator i = users.iterator();
         while (i.hasNext()) {
@@ -23,6 +24,7 @@ public class AuthenticationService implements IAuthenticationService {
         return newUser;
     }
 
+    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
     public User logIn(String username, String password) {
         Iterator i = users.iterator();
         while (i.hasNext()) {
@@ -33,10 +35,6 @@ public class AuthenticationService implements IAuthenticationService {
         }
         return null;
     }
-    // TODO Now: Add a constructor to initialize the users list with the default user
 
-    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
-
-    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
 }
 
